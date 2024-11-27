1723e30a1f4d71898b5ae57a3a82134bfe82a13c
Santijit Kamnak <onopingping@gmail.com>
2024-11-28 01:10:17 +0700

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,6 +5,7 @@ import { databaseUrl } from "./config/database";
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: databaseUrl,
+  synchronize: true,
   logging: false,
   entities: ["src/entity/**/*.ts"],
   migrations: [],
