d8984f4758d4f74af368752ade0251077972b326
onosannnnt <onopingping@gmail.com>
2024-12-06 12:06:27 +0700

    fix : name of entity

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ app.post("/humidity", (req, res) => {
   const logs = new humidity();
   logs.humidity = humidity;
   logs.temperature = temperature;
-  AppDataSource.getRepository(Logs).save(logs);
+  AppDataSource.getRepository(humidity).save(logs);
   if (humidity > 80) {
     sendLineMessage(
       "Ufd79c6344c9a97376eb756961a7830af",
